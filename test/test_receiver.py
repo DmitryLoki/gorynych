68ed85337cf9bd26458f18465bad5240e3835ef6
Boris Tsema <boris@tsema.ru>
2012-11-20 15:03:49 +0400

    test passed

--- test/test_receiver.py
+++ test/test_receiver.py
@@ -24,49 +24,75 @@ class TestEventStore(unittest.TestCase):
 
 
 class ReceiverService(service.Service):
-    def __init__(self, des=DomainEventStore):
+    def __init__(self, des=DomainEventStore, specs=[]):
         self.des = des
+        self.specifications = specs
 
-    def handle_domain_event(self, devent):
-        d = defer.maybeDeferred(self.des.save, devent)
+    def handle_incoming_msg(self, msg):
+        d = defer.maybeDeferred(self.des.save, msg)
         d.addCallbacks(self._choose_event_processor)
         d.addCallback(lambda ev: getattr(ev, 'process'))
         d.addErrback(log.err)
 
-    def _choose_event_processor(self, devent):
-        pass
+    def _build_domain_event(self, msg, klass):
+        return klass(msg)
+
+    def _choose_event_processor(self, msg):
+        for specification in self.specifications:
+            if specification.is_satisfied_by(msg):
+                return self._build_domain_event(msg, specification.class_name)
+        return self._build_domain_event(msg, DomainEvent)
 
 
 class DomainEvent(object):
 
     type = "AbstractDomainEvent"
 
+    def __init__(self, msg):
+        self.msg = msg
+
     def process(self):
         raise NotImplementedError("Process method hasn't been choosed.")
 
 
+class Specification(object):
+
+    def is_satisfied_by(self, msg):
+        raise NotImplementedError("Specification hasn't been implemented.")
+
+class tr203CoordsReceivedSpecification(Specification):
+
+    def is_satisfied_by(self, msg):
+        pass
+
 class TestReceiver(unittest.TestCase):
 
+    def setUp(self):
+        self.receiver_service = ReceiverService()
+        self.receiver_service.startService()
+
+    def tearDown(self):
+        self.receiver_service.stopService()
+
     def test_handle_domain_event(self):
-        re = ReceiverService()
-        devent = DomainEvent()
+        devent = dict(name="ss")
         with mock.patch.object(DomainEventStore, 'save') as ds:
             ds.return_value = devent
             with mock.patch.object(ReceiverService,
                             '_choose_event_processor') as ep:
-                re.handle_domain_event(devent)
+                self.receiver_service.handle_incoming_msg(devent)
                 ds.assert_called_with(devent)
                 ep.assert_called_with(devent)
 
             ds.side_effect = Exception("Bo")
             with mock.patch.object(ReceiverService,
                             '_choose_event_processor') as ep2:
-                re.handle_domain_event(devent)
+                self.receiver_service.handle_incoming_msg(devent)
                 ds.assert_called_with(devent)
                 self.assertFalse(ep2.called)
                 errors = self.flushLoggedErrors(Exception)
 
     def test_choose_event_processor(self):
-        re = ReceiverService()
-        devent = DomainEvent()
-        processed_devent = re._choose_event_processor(devent)
+        devent = dict(name='Basya')
+        processed_devent = self.receiver_service._choose_event_processor(devent)
+        self.assertIsInstance(processed_devent, DomainEvent)

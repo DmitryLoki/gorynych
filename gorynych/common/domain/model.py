b5481ece98c8f325ac120e51a7be16ed4078d8bb
Boris Tsema <boris@tsema.ru>
2013-03-24 19:13:57 +0400

    #2854 IdentifierObject created and tested, AggregateRoot class modified.

--- gorynych/common/domain/model.py
+++ gorynych/common/domain/model.py
@@ -3,6 +3,8 @@ DDD-model specific base classes.
 '''
 import time
 
+from gorynych.common.infrastructure.messaging import DomainEventsPublisher
+
 __author__ = 'Boris Tsema'
 
 class Event(object):
@@ -12,8 +14,22 @@ class Event(object):
 class ValueObject(object):
     pass
 
+
+class IdentifierObject(object):
+
+    def __init__(self, id):
+        if not id:
+            raise AttributeError("No id in IdentifierObject.")
+        self.id = id
+
+    def __eq__(self, other):
+        if isinstance(other, int) or isinstance(other, str):
+            return self.id == other
+        elif isinstance(other, IdentifierObject):
+            return self.id == other.id
+
 class AggregateRoot(object):
-    pass
+    event_publisher = DomainEventsPublisher()
 
 
 class DomainEvent(object):

027cc7d96dd4492e43c2b5f01d4084c50827a9ec
Boris Tsema <boris@tsema.ru>
2013-03-26 15:31:55 +0400

    #2895 ContestFactory created and tested.

--- gorynych/info/domain/contest.py
+++ gorynych/info/domain/contest.py
@@ -1,8 +1,37 @@
 '''
 Contest Aggregate.
 '''
-
-from gorynych.common.domain.model import IdentifierObject
+from gorynych.common.domain.model import IdentifierObject, AggregateRoot
+from gorynych.common.domain.types import Address
 
 class ContestID(IdentifierObject):
     pass
+
+class ContestFactory(object):
+
+    def __init__(self, event_publisher=None):
+        self.event_publisher = event_publisher
+
+    def create_contest(self, id, title, start_time, end_time,
+                       contest_place, contest_country, hq_coords):
+        address = Address(contest_place, contest_country, hq_coords)
+        if not int(start_time) <  int(end_time):
+            raise ValueError("Start time must be less then end time.")
+        title = title.strip().title()
+        if not isinstance(id, ContestID):
+            id = ContestID(id)
+
+        contest = Contest(id, title, start_time, end_time, address)
+        if self.event_publisher:
+            contest.event_publisher = self.event_publisher
+        return contest
+
+
+class Contest(AggregateRoot):
+
+    def __init__(self, id, title, start_time, end_time, address):
+        self.id = id
+        self.title = title
+        self.start_time = start_time
+        self.end_time = end_time
+        self.address = address

77d30d8d791e69635bbca71f7b60883437a4f7a5
Boris Tsema <boris@tsema.ru>
2013-04-09 17:02:55 +0400

    #2927 It's impossible now to save duplicated contest (in pickled repository).

--- gorynych/info/test/helpers.py
+++ gorynych/info/test/helpers.py
@@ -18,12 +18,19 @@ class BaseRepository(object):
             with open(filename, 'wb') as f:
                 cPickle.dump({}, f, -1)
 
+    def find_the_same(self, obj, obj_list):
+        return None
+
     def save(self, obj):
         if not obj:
             return None
         with open(self.filename, 'rb') as f:
             data = cPickle.load(f)
         with open(self.filename, 'wb') as f:
+            item = self.find_the_same(obj, data.values())
+            if item:
+                cPickle.dump(data, f, -1)
+                return item
             data[obj.id] = obj
             cPickle.dump(data, f, -1)
             return obj
@@ -46,7 +53,15 @@ class BaseRepository(object):
 
 class PickleContestRepository(BaseRepository):
     implements(IContestRepository)
-
+    def find_the_same(self, obj, obj_list):
+        for item in obj_list:
+            c1 = item.title == obj.title
+            c2 = item.start_time == obj.start_time
+            c3 = item.end_time == obj.end_time
+            c4 = item.country == obj.country
+            c5 = item.place == obj.place
+            if c1 and c2 and c3 and c4 and c5:
+                return item
 
 class PickleRaceRepository(BaseRepository):
     implements(IRaceRepository)

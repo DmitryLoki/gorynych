782ebf01da0cd54272a13c0eab587f296cd44743
Boris Tsema <boris@tsema.ru>
2013-04-28 18:18:19 +0400

    Fix with environment variable. Now it's develop for odd versions.

--- gorynych/__init__.py
+++ gorynych/__init__.py
@@ -7,6 +7,11 @@ import os
 __version__ = 0.1
 config_file = 'config.yaml'
 
+# Set GOR_ENV to develop if version is odd and not environment variable is set.
+if 10 * __version__ % 2:
+    if not os.getenv('GOR_ENV', None):
+        os.environ['GOR_ENV'] = 'develop'
+
 path_join = os.path.join(os.path.dirname(__file__), config_file)
 if os.path.isfile(path_join):
     with open(path_join, 'r') as f:

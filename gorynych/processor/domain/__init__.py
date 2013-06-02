584b5b29ce33a78568bb8c7db4ed7d120bdd7021
Boris Tsema <boris@tsema.ru>
2013-06-02 15:40:18 +0400

    #3093 track archive reading implemented and tested, I think.

---
+++ gorynych/processor/domain/__init__.py
@@ -0,0 +1,122 @@
+'''
+Processor' Context.
+'''
+import os
+import shutil
+import zipfile
+
+import requests
+
+from gorynych.common.domain.model import ValueObject
+from gorynych import OPTS
+
+
+def get_contest_number(track):
+    if track.endswith('.igc'):
+        words = track.split('.')
+        if len(words) > 2:
+            return words[-2]
+        if len(words) == 2:
+            return str(int(words[0]))
+    elif track.endswith('.kml'):
+        words = track.split('.')
+        return words[-2]
+
+    pass
+
+
+class TrackArchive(ValueObject):
+    '''
+    Download and unpack archive with race tracks.
+    '''
+    def __init__(self, race_id, archive_url, download_dir=None):
+        if not download_dir:
+            download_dir = OPTS['workdir']
+        self.race_id = str(race_id)
+        if not archive_url.endswith('zip'):
+            raise ValueError("I'm waiting for zip-file link but got %s" %
+                             archive_url)
+        self.archive_url = archive_url
+        if not os.path.isdir(download_dir):
+            raise ValueError("%s is not a directory" % download_dir)
+        self.download_dir = download_dir
+
+    def process_archive(self):
+        '''
+        Facade method.
+        @return: filenames from archive
+        @rtype: C{list}
+        '''
+        archive_file = self.download_archive()
+        unpack_dir = os.path.join(self.download_dir, self.race_id)
+        if os.path.isdir(unpack_dir):
+            shutil.rmtree(unpack_dir)
+        os.mkdir(unpack_dir)
+        a_filelist = self.unpack(archive_file, unpack_dir)
+        cont_numbers = self.get_race_paragliders()
+        result = self.find_paragliders_tracks(a_filelist, cont_numbers)
+        return result
+
+    def download_archive(self):
+        r = requests.get(self.archive_url)
+        archive_path = os.path.join(self.download_dir, self.race_id + '.zip')
+        with open(archive_path, 'wb') as f:
+            f.write(r.content)
+        return archive_path
+
+    def unpack(self, archive_file, unpack_dir):
+        namelist = []
+        arc = zipfile.ZipFile(archive_file)
+        for item in arc.infolist():
+            namelist.append(arc.extract(item, unpack_dir))
+        return namelist
+
+    def get_race_paragliders(self):
+        '''
+
+        @return: {contest_number:person_id}
+        @rtype: C{dict}
+        '''
+        url = '/'.join((OPTS['apiurl'], 'race', self.race_id,
+                        'paragliders'))
+        r = requests.get(url)
+        res = r.json()
+        result = dict()
+        for i, item in enumerate(res):
+            cn = item['contest_number']
+            pid = item['person_id']
+            result[cn] = pid
+        return result
+
+    def find_paragliders_tracks(self, a_filelist, paragliders):
+        '''
+        Lookup for files wich has paragliders tracks.
+        @param a_filelist: list with track filenames
+        @type a_filelist: C{list}
+        @param paragliders: {contest_number:person_id}
+        @type paragliders: C{dict}
+        @return:
+        ([{person_id, trackfile, contest_number}, ...],
+        [extra trackfile,],
+         [person_id without tracks,]) -
+         finded tracks for persons,
+         extra tracks,
+        paragliders without tracks.
+        @rtype: C{tuple}
+        '''
+        tracklist = filter(lambda x :
+                       x.endswith('.igc') or x.endswith('.kml'), a_filelist)
+        extra_tracks = []
+        tracks = []
+        for idx, item in enumerate(tracklist):
+            contest_number = get_contest_number(item)
+            if paragliders.has_key(contest_number):
+                tracks.append(dict(person_id=paragliders[contest_number],
+                                   trackfile=item,
+                                   contest_number=contest_number))
+                del paragliders[contest_number]
+            else:
+                extra_tracks.append(item)
+        left_paragliders = paragliders.values()
+        return tracks, extra_tracks, left_paragliders
+
